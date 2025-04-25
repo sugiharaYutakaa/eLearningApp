72bf9f8cec66c8b1bf4e5cb1ae5cda600180d879
sugiharaYutakaa <yutaka.sugihara@g.dreamcareer.co.jp>
2025-04-25 18:39:15 +0900

    db 接続完了

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,41 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-
-// アプリケーションで動作するようにdotenvを設定する
+const mysql = require('mysql2/promise');
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
+};
+const pool = mysql.createPool(dbConfig);
+async function testConnection() {
+  try {
+    const connection = await pool.getConnection();
+    console.log('MySQL データベースに正常に接続しました');
+    connection.release();
+  } catch (err) {
+    console.error('MySQL 接続エラー:', err);
+  }
+}
 
-app.get("/", (request: Request, response: Response) => { 
-  response.status(200).send("Hello World");
+  
+
+const PORT = process.env.EXPRESS_PORT;
+
+app.get("/", async (request: Request, response: Response) => { 
+    const query = "select * from test"
+    const [data] = await pool.query(query);
+  response.status(200).json({ message: "hello Express" , dbMessage:data[0].text});
+
 }); 
 
 app.listen(PORT, () => { 
   console.log("Server running at PORT: ", PORT); 
+  testConnection()
 }).on("error", (error) => {
   // エラーの処理
   throw new Error(error.message);
